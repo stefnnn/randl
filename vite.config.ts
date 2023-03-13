f783f20956639d2034280f4af529a9e9e1155017
Stefan N <stefnnn@users.noreply.github.com>
2023-03-13 11:41:30 +0100

    Add ts paths to vite and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: { src: "./src", "@lib": "./src/lib", "@components": "./src/components" },
+  },
 });
