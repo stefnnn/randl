b299adfada990714d712b476a9248c2e62e7d090
Stefan N <stefnnn@users.noreply.github.com>
2023-03-25 10:41:00 +0100

    Fine-tune layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,11 @@ export const Layout: React.FC<{ children: any }> = ({ children }) => {
   return (
     <div className="p-4 md:p-8">
       <div className="mx-auto max-w-xl">
-        <header className="text-center mb-4">
+        <header className="text-center mb-4 text-primary">
           <h1 className="">RandL</h1>
           <h2>Read and Learn a Foreign Language</h2>
         </header>
-        <main className="bg-white rounded-lg shadow-xl min-h-[500px] p-4">{children}</main>
+        <main className="bg-white rounded-xl shadow-xl min-h-[400px] p-4">{children}</main>
       </div>
     </div>
   );
