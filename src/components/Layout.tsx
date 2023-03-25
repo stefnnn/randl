51d79bf385d04ca829ce7b42bce24d75400748eb
Stefan N <stefnnn@users.noreply.github.com>
2023-03-25 11:47:38 +0100

    Improvements for mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,13 @@ import React from "preact/compat";
 
 export const Layout: React.FC<{ children: any }> = ({ children }) => {
   return (
-    <div className="p-4 md:p-8">
+    <div className="md:p-8">
       <div className="mx-auto max-w-xl">
-        <header className="text-center mb-4 text-primary">
+        <header className="text-center m-4 text-primary">
           <h1 className="">RandL</h1>
           <h2>Read and Learn a Foreign Language</h2>
         </header>
-        <main className="bg-white rounded-xl shadow-xl min-h-[400px] p-4">{children}</main>
+        <main className="bg-white md:rounded-xl md:shadow-xl min-h-[400px] p-4">{children}</main>
       </div>
     </div>
   );
