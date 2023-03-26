ed8126a6cce939a7d2d60ae2d0bbaa26ac5717c9
Stefan N <stefnnn@users.noreply.github.com>
2023-03-26 01:15:52 +0100

    Build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import path from "path";
+// @ts-ignore
+const rootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
   resolve: {
-    alias: { src: "./src", "@lib": "./src/lib", "@components": "./src/components" },
+    alias: {
+      src: path.resolve(rootDir, "src"),
+      "@lib": path.resolve(rootDir, "src/lib"),
+      "@components": path.resolve(rootDir, "src/components"),
+    },
   },
 });
